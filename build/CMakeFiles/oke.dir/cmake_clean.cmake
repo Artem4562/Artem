5e1f44a88a2ac90fb54482c4a1d3ad924e4ed0a4
Secatrillo <106627730+Secatrillo@users.noreply.github.com>
2023-07-23 11:16:11 +0300

    2

---
+++ build/CMakeFiles/oke.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oke.dir/main.cpp.obj"
+  "CMakeFiles/oke.dir/main.cpp.obj.d"
+  "liboke.dll.a"
+  "oke.exe"
+  "oke.exe.manifest"
+  "oke.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oke.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
