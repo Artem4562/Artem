a7ec552377a038833221af371fdb7817f44d85d2
Secatrillo <106627730+Secatrillo@users.noreply.github.com>
2023-08-14 22:29:40 +0300

    da

--- New_York.cpp
+++ New_York.cpp
@@ -39,7 +39,7 @@ void callback(u_char *arg, const struct pcap_pkthdr* pkthdr,
 
 
 int main(int argc, char **argv){
-	string path ="C:\\Users\\mrsic\\Documents\\GitHub\\Artem\\name.txt";
+	string path ="C:\\Users\\galat\\Documents\\GitHub\\Artem\\name.txt";
 	ifstream fin;     //определяю новый поток ввода/вывода потока данных
     char *str = new char;
 	fin.open(path);
@@ -53,7 +53,7 @@ int main(int argc, char **argv){
         cout<<endl;
 		
     
-		fin.close();
+		//fin.close();
 		
 		const u_char* pkt_data;
 		struct pcap_pkthdr *header;
@@ -100,7 +100,7 @@ int main(int argc, char **argv){
 					return -4;
 				}
 
-			cout<<"Recieved Packet Size:                                          ";
+			cout<<"Recieved Packet Size:   ";
 			/*while(pcap_dispatch(fp,-1,callback,NULL)>=0){      //при ловле пакета срабатывает ф-ция callback
 				cout<<string ( to_string(count).length(),'\b'); 
 				cout<<count;
@@ -119,10 +119,11 @@ int main(int argc, char **argv){
 					
 
            		}
-				cout<<string ( to_string(count).length() + 33,'\b');
+				
 				cout<<count;
 				time_start = time(0);
 				printf(" Local Time: %.20s",ctime(&time_start));
+				cout<<string ( to_string(count).length() + 33,'\b');
 				count=0;
 				flag= true;
 				
