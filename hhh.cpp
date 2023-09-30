38bf6c0d6b084a1a18cc651b69f34c3371fb02a1
KirichenkoPV <tanyur4ev@yandex.ru>
2023-10-01 02:49:48 +0300

    qwert

---
+++ hhh.cpp
@@ -0,0 +1,63 @@
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <iostream>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#define GLAD_GL_IMPLEMENTATION
+#include "libraries/glfw/deps/glad/gl.h"
+
+int main(int, char**) {
+    //Инициализация библиотеки GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return EXIT_FAILURE;
+    }
+    //Создаю окно 
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "My window", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Создание контекста OpenGL
+    glfwMakeContextCurrent(window);
+    //Что это не знаю, но без него не работает(
+    gladLoadGL(glfwGetProcAddress);
+    //Частота обновления кадров в приложении такая же, как у монитора
+    glfwSwapInterval(1);
+
+    //Инициализация ImGui и Implot
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    //Инициализация ImGui для работы с OpenGL версии 3.3
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    while (!glfwWindowShouldClose(window)) { //Цикл будет выполняться пока окно не закроется
+        glfwPollEvents();//Обрабатывает все события, которые происходят в окне и позволяет реагировать на них
+
+        //Готовят imGui к отрисовке нового кадра в пользовательском интерфейсе
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Protocol data");
+        ImGui::End();
+
+        ImGui::Render();
+
+
+        //Очищает буфер кадра, обычно для подготовки его к отрисовке нового кадра.
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //Рисует данные пользовательского интерфейса ImGui на текущем буфере кадра OpenGL
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        //Меняет местами буферы кадра GLFW, чтобы отобразить новый кадр на экране.
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
+    
+
+    return 0;
+}
