ab1dceecbc4d0d6f828658a7420ab980c5ef3328
KirichenkoPV <tanyur4ev@yandex.ru>
2023-11-26 20:01:53 +0300

    вертикальное меню

--- GraphicalInterface.cpp
+++ GraphicalInterface.cpp
@@ -16,91 +16,8 @@ bool *flag = new bool; // флаг окно с расширенной инфор
 
 
 void WindowFullInformation(int id,char* svID, bool *flag) {
-    ImVec2 SizeGraph (770,300);
     const char* PackageName = "C++";
-    const char* Stream = "Potok";
     const char* MACdst = "01:0c:cd:01:00:10";
-    const char* MACsrc = "00:50:c2:4f:94:3b";
-    // const char* svID = "0000MU0001";
-    const char* Skippackets = "tut chto-to napisano";
-    ImGui::SetNextWindowPos(ImVec2(0, 0));    
-    ImGui::SetNextWindowSize(ImVec2(800,400));
-    ImGui::Begin("Protocol Data",  nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
-    ImGui::Text("Package Name: %s;  Stream: %s;  MAC dst: %s;  MAC src: %s;", PackageName, Stream, MACdst, MACsrc );
-    ImGui::Text("svID: %s;  Skipped package: %s;",svID, Skippackets);
-    if (ImGui::Button("Close")) flag[id] = false;
-    if (ImPlot::BeginPlot("Graph Ia, Ib, Ic, In", SizeGraph, ImPlotFlags_NoInputs)) {
-        int n = 100; // количество точек на графике
-        float* xs = new float[n];
-        float* ys = new float[n];
-        float step = 2 * M_PI/ n;
-        for (int i = 0; i < n; i++) {
-            xs[i] = i * step;
-            ys[i] = std::sin(xs[i]);
-        }
-        ImPlot::PlotLine("sin(x)", xs, ys, n);
-
-        delete[] xs;
-        delete[] ys;
-        ImPlot::EndPlot();
-    }
-    
-    if (ImPlot::BeginPlot("Graph Ua, Ub, Uc, Un", SizeGraph,  ImPlotFlags_NoInputs)) {
-        int n = 100; // количество точек на графике
-        float* xs = new float[n];
-        float* ys = new float[n];
-        float step = 2 * M_PI/ n;
-        for (int i = 0; i < n; i++) {
-            xs[i] = i * step;
-            ys[i] = std::sin(xs[i]);
-        }
-        ImPlot::PlotLine("sin(x)", xs, ys, n);
-
-        delete[] xs;
-        delete[] ys;
-        ImPlot::EndPlot();
-    }
-    
-    if (ImPlot::BeginPlot("Graph valid values Ua, Ub, Uc, Un", SizeGraph,  ImPlotFlags_NoInputs)) {
-        int n = 100; // количество точек на графике
-        float* xs = new float[n];
-        float* ys = new float[n];
-        float step = 2 * M_PI/ n;
-        for (int i = 0; i < n; i++) {
-            xs[i] = i * step;
-            ys[i] = std::sin(xs[i]);
-        }
-        ImPlot::PlotLine("sin(x)", xs, ys, n);
-
-        delete[] xs;
-        delete[] ys;
-        ImPlot::EndPlot(); 
-    }
-    
-    if (ImPlot::BeginPlot("Graph valid values Ia, Ib, Ic, In", SizeGraph,  ImPlotFlags_NoInputs)) {
-        int n = 100; // количество точек на графике
-        float* xs = new float[n];
-        float* ys = new float[n];
-        float step = 2 * M_PI/ n;
-        for (int i = 0; i < n; i++) {
-            xs[i] = i * step;
-            ys[i] = std::sin(xs[i]);
-        }
-        ImPlot::PlotLine("sin(x)", xs, ys, n);
-
-        delete[] xs;
-        delete[] ys;
-        ImPlot::EndPlot(); 
-    }
-    ImGui::End();
-}
-
-void Monitor(int id,char svID,int a, int b ,bool *flag){
-    
-    const char* Stream = "Potok";
-    const char* MACdst = "01:0c:cd:01:00:10";
-    const char* MACsrc = "00:50:c2:4f:94:3b";
-    // const char* svID = "0000MU0001";
     const char* Ia = "4678";
     const char* Ib = "2023";
     const char* Ic = "7698";
@@ -109,15 +26,44 @@ void Monitor(int id,char svID,int a, int b ,bool *flag){
     const char* Ub = "1337";
     const char* Uc = "3654";
     const char* Un = "5436";
-    ImGui::SetNextWindowPos(ImVec2(a, b)); // Указывает конкретную область, в которой должно появиться окно
-    ImGui::SetNextWindowSize(ImVec2(400,165));
-    ImGui::Begin(&svID,  nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
-    ImGui::Text("Stream: %s;  MAC dst: %s; ", Stream, MACdst);
-    ImGui::Text("MAC src: %s;  svID: %s;", MACsrc,&svID);
+    ImGui::SetNextWindowPos(ImVec2(0, 0));    
+    ImGui::SetNextWindowSize(ImVec2(800,400));
+    ImGui::Begin("Protocol Data",  nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
+    ImGui::Text("svID: %s; Package Name: %s; MAC dst: %s;  ", svID, PackageName, MACdst);
     ImGui::Text("Ia= %s;  Ua= %s;",Ia,Ua);
     ImGui::Text("Ib= %s;  Ub= %s;",Ib,Ub);
     ImGui::Text("Ic= %s;  Uc= %s;",Ic,Uc);
     ImGui::Text("In= %s;  Un= %s;",In,Un);
+    if (ImGui::Button("Close")) flag[id] = false;
+
+    
+    // if (ImPlot::BeginPlot("Graph valid values Ia, Ib, Ic, In", SizeGraph,  ImPlotFlags_NoInputs)) {
+    //     int n = 100; // количество точек на графике
+    //     float* xs = new float[n];
+    //     float* ys = new float[n];
+    //     float step = 2 * M_PI/ n;
+    //     for (int i = 0; i < n; i++) {
+    //         xs[i] = i * step;
+    //         ys[i] = std::sin(xs[i]);
+    //     }
+    //     ImPlot::PlotLine("sin(x)", xs, ys, n);
+
+    //     delete[] xs;
+    //     delete[] ys;
+    //     ImPlot::EndPlot(); 
+    // }
+    ImGui::End();
+}
+
+void Monitor(int id,char svID,int a, int b ,bool *flag){
+    const char* Stream = "Potok";
+    const char* MACdst = "01:0c:cd:01:00:10";
+    // const char* svID = "0000MU0001";
+    ImGui::SetNextWindowPos(ImVec2(a, b)); // Указывает конкретную область, в которой должно появиться окно
+    ImGui::SetNextWindowSize(ImVec2(400,165));
+    ImGui::Begin(&svID,  nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
+    ImGui::Text("svID: %s; Stream: %s;  MAC dst: %s; ", &svID, Stream, MACdst);
+
     if (ImGui::Button("Open")) flag[id] = true;
     // if (ImGui::IsMouseClicked(ImGuiMouseButton_Left) && ImGui::IsWindowHovered()) {
     //     flag = true;
@@ -126,6 +72,64 @@ void Monitor(int id,char svID,int a, int b ,bool *flag){
         WindowFullInformation(id,&svID, flag);
     }
     ImGui::End();   
+} 
+
+void Common_Window(bool *flag){
+    ImGui::SetNextWindowPos(ImVec2(0,0));
+    ImGui::SetNextWindowSize(ImVec2(400,800));
+    ImGui::Begin("Streams SV",  nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
+    ImGui::SetCursorPosX(200.0f);
+    ImGui::Text("X streams detected");
+    ImGui::SetCursorPosX(0.0f);
+    // if (ImGui::Button("Main Menu", ImVec2(150, 50))) flag[id] = false;
+    // while (){
+    //     Monitor(0,'1',0, 0,flag);
+    // }
+
+}
+
+void Main_Menu(bool *flag){
+    ImGui::SetNextWindowPos(ImVec2(0,0)); // Указывает конкретную область, в которой должно появиться окно
+    ImGui::SetNextWindowSize(ImVec2(400,800));
+    ImGui::Begin(" ",  nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove| ImGuiWindowFlags_NoInputs);
+    ImVec2 sizewindow = ImGui::GetWindowSize();
+    ImVec2 sizetext = ImGui::CalcTextSize("Main Menu");
+    float posX = (sizewindow.x - sizetext.x) * 0.5f;
+    ImGui::SetCursorPosX(posX);
+    ImGui::Text("Main Menu"); 
+    ImGui::End();
+
+    ImGui::SetNextWindowPos(ImVec2(0,45));
+    ImGui::SetNextWindowSize(ImVec2(200,200));
+    ImGui::Begin("Streams SV",  nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
+    if (ImGui::Button("Streams SV", ImVec2(150, 50))) ;
+    // if(flag[id]){
+    //     Common_Window();
+    // }
+    ImGui::End(); 
+
+    ImGui::SetNextWindowPos(ImVec2(200,45));
+    ImGui::SetNextWindowSize(ImVec2(200,200));
+    ImGui::Begin("Streams GOOSE ",  nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
+    if (ImGui::Button("Streams GOOSE", ImVec2(150, 50))) ;
+    ImGui::End();  
+    
+    ImGui::SetNextWindowPos(ImVec2(0,245));
+    ImGui::SetNextWindowSize(ImVec2(200,200));
+    ImGui::Begin("Generator SV ",  nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
+    if (ImGui::Button("Generator SV", ImVec2(150, 50))) ;
+    ImGui::End();
+    
+    ImGui::SetNextWindowPos(ImVec2(200,245));
+    ImGui::SetNextWindowSize(ImVec2(200,200));
+    ImGui::Begin(" Generator GOOSE",  nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
+    if (ImGui::Button("Generator GOOSE", ImVec2(150, 50))) ;
+    ImGui::End();
+    
+    // if(flag[id]){
+    //     WindowFullInformation(id,&svID, flag);
+    // }
+       
     
 } 
 
@@ -143,7 +147,7 @@ int main(int, char**) {
         return EXIT_FAILURE;
     }
     //Создаю окно 
-    GLFWwindow* window = glfwCreateWindow(800, 400, "My window", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(400, 800, "My window", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -176,13 +180,14 @@ int main(int, char**) {
         
         // Вызывает функцию
         // WindowFullInformation(&IAMY,&TIMEX);
+        // Main_Menu();
         
         Monitor(0,'1',0, 0,flag);
-        Monitor(1,'2',400, 0,flag);
-        Monitor(2,'3',0, 200,flag);
-        Monitor(3,'4',400, 200,flag);
-        Monitor(4,'5',0, 400,flag);
-        Monitor(5,'6',400, 400,flag);
+        // Monitor(1,'2',400, 0,flag);
+        // Monitor(2,'3',0, 200,flag);
+        // Monitor(3,'4',400, 200,flag);
+        // Monitor(4,'5',0, 400,flag);
+        // Monitor(5,'6',400, 400,flag);
 
         // Monitor();
 
