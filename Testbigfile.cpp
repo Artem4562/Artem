dbc7cd85b6297fc10980a3890b1d41379d62bb02
Secatrillo <mrsicer1@gmail.com>
2024-03-17 01:51:09 +0300

    Goods

--- Testbigfile.cpp
+++ Testbigfile.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <DFT.hpp>
 #include <time.h>
+#include <unistd.h>
 #define LINE_LEN 16
 
 using namespace std;
@@ -28,7 +29,16 @@ static std::vector<SV_PROT_D> Result;
 
 int main(int argc, char **argv)
 {	
-	kek = 10000*(int(argv[2][0])-48)+1000*int((argv[2][1])-48)+100*int((argv[2][2])-48)+10*int((argv[2][3])-48)+int((argv[2][4])-48);
+
+	// accept signal from VSCode for pausing/stopping
+    char *sudo_uid = getenv("SUDO_UID");
+    if (sudo_uid)
+        setresuid(0, 0, atoi(sudo_uid));
+
+    printf("uid = %d\n", getuid());
+
+
+
 	pcap_t *fp;
 	char errbuf[PCAP_ERRBUF_SIZE];
 	struct pcap_pkthdr *header;
@@ -45,7 +55,7 @@ int main(int argc, char **argv)
 	char timestr[16];
 	int inum;
 
-	
+	std::cin>>(kek);
 	    /* Retrieve the device list on the local machine */
     if (pcap_findalldevs(&alldevs, errbuf) == -1)
     {
@@ -84,82 +94,49 @@ int main(int argc, char **argv)
     for(dev=alldevs, i=0; i< inum-1 ;dev=dev->next, i++);
     
     /* Open the device */
-    if ( (fp= pcap_open_live(dev->name,          // name of the device
+    if ( (fp= pcap_open_live(dev->name,         // name of the device
                               65536,            // portion of the packet to capture. 
-                                                // 65536 guarantees that the whole packet will be captured on all the link layers
-                              1000,             // read timeout
-                              NULL,             // authentication on the remote machine
+                              true,             // 65536 guarantees that the whole packet will be captured on all the link layers
+                              1000,             // read timeout            
                               errbuf            // error buffer
                               ) ) == NULL)
     {
         fprintf(stderr,"\nUnable to open the adapter. %s is not supported by WinPcap\n", dev->name);
+		std::cout<<'\n'<<errbuf;
         /* Free the device list */
         pcap_freealldevs(alldevs);
         return -1;
     }
+
+	if (pcap_datalink(fp) != DLT_EN10MB) 
+		{
+			fprintf(stderr, "Device %s doesn't provide Ethernet headers -not  supported\n", dev->name);
+			return(2);
+		}
+
+	if((res = pcap_compile(fp, &fcode, "not udp and not ip and ether[12]=136 and ether[13]=186", 1, 0)) < 0) //составление фльтра 
+		{	
+			cout<<"\nError compiling filter: "<< res <<'\n';
+			pcap_close(fp);
+			return -3;
+		}
+
+		//set the filter
+	if((res = pcap_setfilter(fp, &fcode))<0)   //применение фильтра 
+		{
+			cout<<"\nError setting the filter: "<< res <<'\n';
+			pcap_close(fp);
+			return -4;
+		}
+
     
-    printf("\nlistening on %s...\n", dev->description);
+    printf("\nlistening on %s...\n", dev->name);
     
     /* At this point, we don't need any more the device list. Free it */
     pcap_freealldevs(alldevs);
     
-    /* Retrieve the packets */
-    while((res = pcap_next_ex( fp, &header, &pkt_data)) >= 0){
-        
-        if(res == 0)
-            /* Timeout elapsed */
-            continue;
-        
-        /* convert the timestamp to readable format */
-        local_tv_sec = header->ts.tv_sec;
-        localtime_r(&local_tv_sec,&ltime);
-        strftime( timestr, sizeof timestr, "%H:%M:%S", &ltime);
-        
-        printf("%s,%.6d len:%d\n", timestr, header->ts.tv_usec, header->len);
-    }
     
-    if(res == -1){
-        printf("Error reading the packets: %s\n", pcap_geterr(fp));
-        return -1;
-    }
-    
-    return 0;
-	
-	
-		/* Open the capture file */
-	string name ;
-	name = argv[1];
-	if ((	fp = pcap_open_live(dev->name,     // name of the device
-			BUFSIZ,
-			0,
-			10000,
-			errbuf							// error buffer
-			)) == NULL);
-	{
-		fprintf(stderr,"\nUnable to open the file: %s.\n", errbuf);
-		return -1;
-	}
-
-
-		if((res = pcap_compile(fp, &fcode, "not udp", 1, 0)) < 0) //составление фльтра 
-			{	
-				cout<<"\nError compiling filter: "<< res <<'\n';
-				pcap_close(fp);
-				return -3;
-			}
-
-			//set the filter
-		if((res = pcap_setfilter(fp, &fcode))<0)   //применение фильтра 
-			{
-				cout<<"\nError setting the filter: "<< res <<'\n';
-				pcap_close(fp);
-				return -4;
-			}
-
-
-
-
-	if(!argv[2]<10) dispatcher = dispatcher_handler1; 
+	if(!kek) dispatcher = dispatcher_handler1; 
 	else dispatcher = dispatcher_handler2;
 	pcap_loop(fp,0,dispatcher,NULL);
 	
