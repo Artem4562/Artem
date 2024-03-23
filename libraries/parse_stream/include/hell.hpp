7a6825af168ed4513f428a8625e3621968ec7f29
Secatrillo <mrsicer1@gmail.com>
2024-03-23 20:11:20 +0300

    ok

--- libraries/parse_stream/include/hell.hpp
+++ libraries/parse_stream/include/hell.hpp
@@ -34,51 +34,50 @@ typedef struct SV_PROT_NF_I{
     private:
     std::chrono::system_clock::time_point saved_time = std::chrono::system_clock::now();
     public:
-    std::string Destination = "";
-    std::string Source = "";
-    unsigned short AppID = 0;
-    std::vector<char>svID = {};
-    unsigned char id = 0;
+    std::string Destination;
+    std::string Source;
+    unsigned short AppID;
+    std::vector<char>svID;
+    unsigned char id;
     bool opened = false;
-    std::string condition = "";
+    std::string condition;
     int smt_counter = 0;
     
 
-    // SV_PROT_NF_I(   unsigned char PDestination[6], unsigned char PSource[6], 
-    //                 unsigned short PAppID, std::vector<char>PsvID, unsigned char Pid, 
-    //                 bool Popened, std::string Pcondition, int Psmt_counter)
-    // {
-    //     for(int i = 0;i<6;i++){
-    //         Destination+= std::to_string(PDestination[i]);
-    //         Source+= std::to_string(PSource[i]);
-    //         if(i<5){
-    //             Destination+= ":";
-    //             Source+= ":";
-    //         }
-    //     }
-    //     AppID = PAppID;
-    //     // std::copy(PsvID.begin(), PsvID.end(), svID.begin());
-        
-    //     id = u_char(Pid);
-    //     opened = Popened;
-    //     condition = Pcondition;
-    //     smt_counter = Psmt_counter;
-    // }
+    SV_PROT_NF_I(   unsigned char PDestination[6], unsigned char PSource[6], 
+                    unsigned short PAppID, std::vector<char>PsvID, unsigned char Pid, 
+                    bool Popened, std::string Pcondition, int Psmt_counter)
+    {
+        for(int i = 0;i<6;i++){
+            Destination+= std::to_string(PDestination[i]);
+            Source+= std::to_string(PSource[i]);
+            if(i<5){
+                Destination+= ":";
+                Source+= ":";
+            }
+        }
+        AppID = PAppID;
+        svID = PsvID;
+        id = u_char(Pid);
+        opened = Popened;
+        condition = Pcondition;
+        smt_counter = Psmt_counter;
+    }
 
-    // SV_PROT_NF_I(): SV_PROT_NF_I({},{},0,{},0,0,"",0){     };
-    // SV_PROT_NF_I(   unsigned char PDestination[6], unsigned char PSource[6], 
-    //                 unsigned short PAppID, std::vector<char>PsvID, int Pid)
-    //                 :SV_PROT_NF_I(  PDestination, PSource, 
-    //                                 PAppID, PsvID,Pid, 
-    //                                 opened, condition,0){     };
-    // SV_PROT_NF_I(   std::string Pcondition)
-    //                 :SV_PROT_NF_I(  {}, {}, 
-    //                                 AppID, svID,id, 
-    //                                 opened, Pcondition,0){     };
-    // SV_PROT_NF_I(   int Psmt_counter)
-    //                 :SV_PROT_NF_I(  {}, {}, 
-    //                                 AppID, svID,id, 
-    //                                 opened, condition,Psmt_counter){     };
+    SV_PROT_NF_I(): SV_PROT_NF_I({},{},0,{},0,0,"",0){     };
+    SV_PROT_NF_I(   unsigned char PDestination[6], unsigned char PSource[6], 
+                    unsigned short PAppID, std::vector<char>PsvID, int Pid)
+                    :SV_PROT_NF_I(  PDestination, PSource, 
+                                    PAppID, PsvID,Pid, 
+                                    opened, condition,0){     };
+    SV_PROT_NF_I(   std::string Pcondition)
+                    :SV_PROT_NF_I(  {}, {}, 
+                                    AppID, svID,id, 
+                                    opened, Pcondition,0){     };
+    SV_PROT_NF_I(   int Psmt_counter)
+                    :SV_PROT_NF_I(  {}, {}, 
+                                    AppID, svID,id, 
+                                    opened, condition,Psmt_counter){     };
 
     
 
