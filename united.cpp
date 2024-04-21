ae7c8af942f49b91c46e4b42c1e613c11173edc2
Secatrillo <mrsicer1@gmail.com>
2024-04-21 08:35:10 +0300

    not working(

--- united.cpp
+++ united.cpp
@@ -7,7 +7,9 @@
 #include <DFT.hpp>
 #include <time.h>
 #include <unistd.h>
-#include "libraries/glfw/deps/glad/gl.h"
+#include <GL/gl.h>
+#include <GL/glext.h>
+//#include "libraries/glfw/deps/glad/gl.h"
 #include <GLFW/glfw3.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
@@ -18,6 +20,10 @@
 #include <fstream>
 #include <queue>
 #include <thread_manager.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 
 
 using namespace std;
@@ -263,9 +269,46 @@ typedef struct{
 
         
         ImGui::End();
-    } 
+    }
+
+    
 
 }Display;
+
+
+bool LoadTextureFromFile(const char* filename, GLuint* out_texture, int* out_width, int* out_height)
+{
+    // Load from file
+    int image_width = 0;
+    int image_height = 0;
+    unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
+    if (image_data == NULL)
+        return false;
+
+    // Create a OpenGL texture identifier
+    GLuint image_texture;
+    glGenTextures(1, &image_texture);
+    glBindTexture(GL_TEXTURE_2D, image_texture);
+
+    // Setup filtering parameters for display
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
+
+    // Upload pixels into texture
+#if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+#endif
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+    stbi_image_free(image_data);
+
+    *out_texture = image_texture;
+    *out_width = image_width;
+    *out_height = image_height;
+
+    return true;
+} 
 
 typedef struct packet_handler{
     pthread_mutex_t mutex_DK;
@@ -336,9 +379,9 @@ typedef struct packet_handler{
     }    
 
 
-
-
     
+
+        
 
 }packet_handler;
 
@@ -669,6 +712,11 @@ void * draw(void* args){
     }
 
     
+    int my_image_width = 0;
+    int my_image_height = 0;
+    GLuint my_image_texture = 0;
+    bool ret = LoadTextureFromFile("../image/logo2.jpg", &my_image_texture, &my_image_width, &my_image_height);
+    IM_ASSERT(ret);
 
     while (!glfwWindowShouldClose(window)) { //Цикл будет выполняться пока окно не закроется
         glfwPollEvents();//Обрабатывает все события, которые происходят в окне и позволяет реагировать на них
